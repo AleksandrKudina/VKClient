e6eeb87f2498441ae89484e1b0432715074d2d7f
Aleksandr Kudina <kudina.aleksandr@gmail.com>
2018-07-14 22:28:13 +0300

    login vc

--- VKClient/VKClient/ViewControllers/LoginScreen/VKCLoginScreenVC.h
+++ VKClient/VKClient/ViewControllers/LoginScreen/VKCLoginScreenVC.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  VKCLoginScreenVC.h
 //  VKClient
 //
 //  Created by Кудина  on 14.07.2018.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface VKCLoginScreenVC : UIViewController
 
 @end
-
