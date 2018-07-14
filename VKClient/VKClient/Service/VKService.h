1dbe6c2fd14caad3ba9b012ac2db906a56acd1a8
Aleksandr Kudina <kudina.aleksandr@gmail.com>
2018-07-14 22:28:24 +0300

    add service

---
+++ VKClient/VKClient/Service/VKService.h
@@ -0,0 +1,13 @@
+//
+//  VKService.h
+//  VKClient
+//
+//  Created by Кудина  on 14.07.2018.
+//  Copyright © 2018 KudinaAG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VKService : NSObject
+
+@end
---
+++ VKClient/VKClient/Service/VKService.m
@@ -0,0 +1,15 @@
+//
+//  VKService.m
+//  VKClient
+//
+//  Created by Кудина  on 14.07.2018.
+//  Copyright © 2018 KudinaAG. All rights reserved.
+//
+
+#import "VKService.h"
+
+@implementation VKService
+
+
+
+@end
