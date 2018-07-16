85a20ca2dfe4c34523f0179837557371decc4341
Aleksandr Kudina <kudina.aleksandr@gmail.com>
2018-07-16 11:26:46 +0300

    add entity for news object

---
+++ VKClient/VKClient/CoreLayer/Entity/VKCNewsEntity.h
@@ -0,0 +1,17 @@
+//
+//  VKCNewsEntity.h
+//  VKClient
+//
+//  Created by Кудина  on 16.07.2018.
+//  Copyright © 2018 KudinaAG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol IVKCNewsEntity
+
+@end
+
+@interface VKCNewsEntity : NSObject <IVKCNewsEntity>
+
+@end
---
+++ VKClient/VKClient/CoreLayer/Entity/VKCNewsEntity.m
@@ -0,0 +1,13 @@
+//
+//  VKCNewsEntity.m
+//  VKClient
+//
+//  Created by Кудина  on 16.07.2018.
+//  Copyright © 2018 KudinaAG. All rights reserved.
+//
+
+#import "VKCNewsEntity.h"
+
+@implementation VKCNewsEntity
+
+@end
